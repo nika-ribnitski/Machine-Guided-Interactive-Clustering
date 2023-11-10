7342ccfe36d4b504fc5a990c15b0a9d5ef793408
nika-ribnitski <71713426+nika-ribnitski@users.noreply.github.com>
2023-11-10 13:20:50 -0500

    default values and clearCookies function added (#4)
    Co-authored-by: AdJansen <adamjansen3@gmail.com>

--- interactive-constrained-clustering/src/components/pages/landingPage/inputForm/cookieManager/readCookies.js
+++ interactive-constrained-clustering/src/components/pages/landingPage/inputForm/cookieManager/readCookies.js
@@ -1,10 +1,14 @@
 // This function returns the cookies in a manageable state
+
+import { clearCookies } from "./clearCookies";
+
 // To be used in Python.js to set inital values for FormInput
 export function readCookies() {
     var pairs = document.cookie.split(";");
     var cookies = {};
 
     if (pairs.length > 1) {
+      // if cookies exist, read them
       var i = 0
       for (; i<pairs.length-1; i++){
         var pair = pairs[i].split("=");
@@ -15,6 +19,15 @@ export function readCookies() {
       var checked = pairs[i].split("=");
       var checkedArray = checked[1].split(",");
       cookies[(checked[0]+'').trim()] = checkedArray;
+    } else {
+      // if no cookies are set, return these default values
+      cookies = {
+        checked: [ "INNE", "LOF", "SIL", "COPOD", "IF" ],
+        maxConstraintPercent: "0.000003",
+        numberOfClusters: "3",
+        questionsPerIteration: "20",
+        reduction_algorithm: "PCA"
+      }
     }
     
     return cookies;
