ce5cd13e47cd93392a61902eedbdcb6f83725640
AdJansen <adamjansen3@gmail.com>
2023-11-10 13:20:00 -0500

    default values and clearCookies function added

---
+++ interactive-constrained-clustering/src/components/pages/landingPage/inputForm/cookieManager/clearCookies.js
@@ -0,0 +1,11 @@
+// Call this function when you want to clear the cookies
+export function clearCookies() {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
